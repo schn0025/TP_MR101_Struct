a658eee322aa37d82b754f65f674d0074843209a
Arthur Schneider <arthur.schneider@etudiant.univ-reims.fr>
2022-11-16 10:30:02 +0100

    ajout de la fonction compter

--- TP1/tp1.py
+++ TP1/tp1.py
@@ -6,3 +6,10 @@ def getRandomList(taille: int, mn: int, mx: int) -> list:
     for i in range(taille):
         lst.append(randint(mn, mx - 1))
     return lst
+
+def compter(lst: list, elmt: int) -> int:
+    nb = 0
+    for e in lst:
+        if e == elmt:
+            nb += 1
+    return nb
--- main_tp1.py
+++ main_tp1.py
@@ -1,5 +1,10 @@
 # main_tp1.py
-from TP1.tp1 import getRandomList
+from TP1.tp1 import *
+
 
 for i in range(5):
     print(getRandomList(10, -20, 20))
+
+liste = getRandomList(10, -5, 6)
+print(compter(liste, 0))
+print(compter(liste, 10))
