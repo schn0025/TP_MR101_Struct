c9f5113bf8f13daf8661d6387a6c88c12d8a26c6
Arthur Schneider <arthur.schneider@etudiant.univ-reims.fr>
2022-11-16 10:49:40 +0100

    Ajout de la fonction contient : tests OK

--- TP1/tp1.py
+++ TP1/tp1.py
@@ -7,9 +7,20 @@ def getRandomList(taille: int, mn: int, mx: int) -> list:
         lst.append(randint(mn, mx - 1))
     return lst
 
+
 def compter(lst: list, elmt: int) -> int:
     nb = 0
     for e in lst:
         if e == elmt:
             nb += 1
     return nb
+
+
+def contient(lst: list, nb: int) -> bool:
+    rep = False
+    i = 0
+    while not rep and i < len(lst):
+        if lst[i] == nb:
+            rep = True
+        i += 1
+    return rep
