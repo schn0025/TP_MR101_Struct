dd400bfefee33f26c0e74127cc90350823d1af74
Arthur Schneider <arthur.schneider@etudiant.univ-reims.fr>
2022-11-23 10:19:40 +0100

    Ajout de la fonction triBulles : tests OK

--- TP2/tp2.py
+++ TP2/tp2.py
@@ -3,8 +3,24 @@ def estTrie(lst: list) -> bool:
     i = 0
     if not lst:
         rep = True
-    while i < len(lst) - 1 and lst[i] < lst[i + 1]:
+    while i < len(lst) - 1 and lst[i] <= lst[i + 1]:
         i += 1
     if i == len(lst) - 1:
         rep = True
     return rep
+
+def echanger(lst: list, i1: int, i2: int) -> None:
+    lst[i1], lst[i2] = lst[i2], lst[i1]
+    return None
+
+def triBulles(lst: list) -> None:
+    etape = 1
+    modif = -1
+    while modif != 0:
+        modif = 0
+        for i in range(len(lst)-1):
+            if lst[i] > lst[i+1]:
+                echanger(lst, i, i+1)
+                modif += 1
+        etape += 1
+    return None
--- main_tp2.py
+++ main_tp2.py
@@ -1,4 +1,13 @@
 from TP1.tp1 import getRandomList
-from TP2.tp2 import estTrie
+from TP2.tp2 import estTrie, echanger, trieBulles
 print(estTrie([1]))
 print(estTrie([2, 1]))
+lst = [1, 2, 3]
+print(lst)
+echanger(lst, 0, 2)
+print(lst)
+
+lst = [12, 54, 2, 35, 6, 9, 87, 4, 5]
+print(lst)
+trieBulles(lst)
+print(lst)
