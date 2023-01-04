5af5edf10928f9c3e5b53202cad68c565338b02f
Arthur Schneider <arthur.schneider@etudiant.univ-reims.fr>
2023-01-04 10:25:16 +0100

    Ajout de la fonction getPrix

---
+++ TP5/tp5.py
@@ -0,0 +1,5 @@
+def getPrix(p: dict, a: str) -> int:
+    rep = 0.0
+    if a in p:
+        rep= p[a]
+    return rep
--- main_tp5.py
+++ main_tp5.py
@@ -1,7 +1,11 @@
-articles = ['Pommes', 'Poires', 'Fraises', 'Bananes', 'Oranges', 'Clémentines', 'Endives', 'Laitues']
-prix =[5.27, 7.12, 4.98, 3.46, 4.02, 5.27, 1.67,2.78]
-dico = {}
-for i in range(len(prix)):
-    dico[articles[i]]=prix[i]
+from TP5.tp5 import getPrix
 
-print(dico)
+articles = ['Pommes', 'Poires', 'Fraises', 'Bananes', 'Oranges', 'Clémentines', 'Endives', 'Laitues']
+prix = [5.27, 7.12, 4.98, 3.46, 4.02, 5.27, 1.67, 2.78]
+panier = {}
+for i in range(len(prix)):
+    panier[articles[i]] = prix[i]
+
+# print(getPrix(panier, "Poires")) # Devrait afficher 7.12
+# print(getPrix(panier, "Clémentines")) # Devrait afficher 5.27
+# print(getPrix(panier, "Cerises")) # Devrait afficher 0.0
